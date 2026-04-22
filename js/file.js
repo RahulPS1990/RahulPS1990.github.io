083f49110827650697f67d6215823826a3003bf8
Rahul <ptd2509@students.iitmandi.ac.in>
2026-04-22 16:55:00 +0530

    update

--- js/file.js
+++ js/file.js
@@ -1,8 +1,9 @@
 fetch('count.xml')
   .then(response => response.text())
   .then(str => new DOMParser().parseFromString(str, "text/xml"))
-  .then(xml => {
-    const count = xml.getElementsByTagName("count")[0]?.textContent || "0";
-    document.getElementById("output").textContent = count;
+  .then(data => {
+    let count = data.getElementsByTagName("count")[0].textContent;
+    let output = count;
+    document.getElementById("output").innerHTML = output;
   })
   .catch(err => console.error("Error loading XML:", err));
